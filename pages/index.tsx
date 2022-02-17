9bd781c2f3052a8f27b82ab91b77c4c3cda40cc4
Adina Totorean <adina.totorean@hcl.com>
2022-02-17 16:49:55 +0200

    Initialized index page

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Instagram 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Header */}
+
+      {/* Feed */}
+
+      {/* Modal */}
+    </div>
+  )
+}
