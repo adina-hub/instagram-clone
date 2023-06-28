2da5f56847f4cb3b4a0f9cb387391f2e09427a80
Adina Totorean <adina.totorean@hcl.com>
2023-06-28 17:55:39 +0300

    Building the Stories section on the Feed

---
+++ components/Stories.js
@@ -0,0 +1,32 @@
+import { faker } from '@faker-js/faker';
+import { useState, useEffect } from "react"
+import Story from './Story'
+
+function Stories() {
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const suggestions = [...Array(20)].map((_, i) => ({
+      userId: faker.datatype.uuid(),
+      username: faker.internet.userName(),
+      avatar: faker.image.avatar(),
+    }));
+
+    setSuggestions(suggestions);
+  }, [])
+  
+
+  return (
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black ">
+      {suggestions.map( (profile) => (
+        <Story 
+            key={profile.userId} 
+            image={profile.avatar} 
+            username={profile.username} 
+        />
+      ))}
+    </div>
+  )
+}
+
+export default Stories;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
 import Head from 'next/head'
 import { Header } from '../components/Header.js'
+import Feed from '../components/Feed.js'
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
         <title>Instagram 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
       <Header />
-      {/* Feed */}
 
+      <Feed />
       {/* Modal */}
     </div>
   )
