642b8bd02fa6c0bec471c3165a814ae7b739e925
Adina Totorean <adina.totorean@hcl.com>
2023-06-28 16:01:33 +0300

    Added Header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { Header } from '../components/Header.js'
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
       </Head>
 
       {/* Header */}
-
+      <Header />
       {/* Feed */}
 
       {/* Modal */}
