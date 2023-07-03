ebec06a9e2b1daced531678e81ccbc2b0701cfb3
Adina Totorean <adina.totorean@hcl.com>
2023-07-03 13:03:04 +0300

    Modified Posts component to get firebase data

--- components/Posts.js
+++ components/Posts.js
@@ -1,32 +1,30 @@
+import { useState, useEffect } from 'react'
 import Post from './Post'
-
-const posts = [
-  {
-    id: '123',
-    username: 'adaa23',
-    userImg: 'https://links.papareact.com/3ke',
-    img: 'https://links.papareact.com/3ke',
-    caption: 'Subscribe to this'
-  },
-  {
-    id: '822',
-    username: 'sonny',
-    userImg: 'https://links.papareact.com/3ke',
-    img: 'https://links.papareact.com/3ke',
-    caption: 'This is awesome'
-  }
-]
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { db } from '../firebase'
 
 function Posts() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(
+    () =>
+      onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), snapshot => {
+        setPosts(snapshot.docs)
+      }
+    ),
+  [db]
+  )
+  
+
   return (
     <div>
       { posts.map( (post) => (
         <Post key={post.id} 
               id={post.id}
-              username={post.username} 
-              userImg={post.userImg}
-              img={post.img}
-              caption={post.caption} />
+              username={post.data().username} 
+              userImg={post.data().profileImage}
+              img={post.data().image}
+              caption={post.data().caption} />
       ) ) }
     </div>
   )
