463f6741640343a490990dccde2d811dbe53f3b8
Adina Totorean <adina.totorean@hcl.com>
2023-07-03 13:05:26 +0300

    Modified the Post component

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,10 @@
 import { BookmarkIcon, ChatIcon, DotsHorizontalIcon, EmojiHappyIcon, HeartIcon, PaperAirplaneIcon } from "@heroicons/react/outline"
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid"
+import { useSession } from "next-auth/react"
 
 function Post({ id, username, userImg, img, caption }) {
+  const {data: session} = useSession()
+
   return (
     <div className="bg-white my-7 rounded-sm border">
       <div className="flex items-center p-5">
@@ -10,25 +13,29 @@ function Post({ id, username, userImg, img, caption }) {
         <DotsHorizontalIcon className="h-5" />
       </div>
       <img src={img} alt="img" className="object-cover w-full" />
-      <div className="flex justify-between px-4 pt-4">
-        <div className="flex space-x-4">
-          <HeartIcon className="btn" />
-          <ChatIcon className="btn" />
-          <PaperAirplaneIcon className="btn" />
+      { session && (
+        <div className="flex justify-between px-4 pt-4">
+          <div className="flex space-x-4">
+            <HeartIcon className="btn" />
+            <ChatIcon className="btn" />
+            <PaperAirplaneIcon className="btn" />
+          </div>
+          <BookmarkIcon className="btn" />
         </div>
-        <BookmarkIcon className="btn" />
-      </div>
+      )}
       <p className="p-5 truncate">
         <span className="font-bold mr-1">{username}</span>
         {caption}
       </p>
-      <form className="flex items-center p-4">
-        <EmojiHappyIcon className="h-7"/>
-        <input type="text" 
+      { session && (
+        <form className="flex items-center p-4">
+          <EmojiHappyIcon className="h-7"/>
+          <input type="text" 
                placeholder="Add a comment..." 
                className="border-none flex-1 focus:ring-0"/>
-        <button className="font-semibold text-blue-400">Post</button>
-      </form>
+          <button className="font-semibold text-blue-400">Post</button>
+        </form>
+      )} 
     </div>
   )
 }
