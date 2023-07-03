61faeb840114036f8accf06cbca7c5110f731256
Adina Totorean <adina.totorean@hcl.com>
2023-07-03 15:35:01 +0300

    Changed Feed visibility

--- components/Feed.js
+++ components/Feed.js
@@ -11,12 +11,12 @@ function Feed() {
     <main className={`grid grid-cols-1 md:grid-cols-2 md:max-w-3xl
      xl:grid-cols-3 xl:max-w-6xl mx-auto
      ${!session && "!grid-cols-1 !max-w-3xl"}`}>
-      { session && (
-          <section className="col-span-2">
-            <Stories />
-            <Posts />
-          </section>
-      )}
+       { session && (
+        <section className="col-span-2">
+          <Stories />
+          <Posts />
+        </section>
+       )}
 
       { session && (
         <section className='hidden xl:inline-grid md:col-span-1'>
